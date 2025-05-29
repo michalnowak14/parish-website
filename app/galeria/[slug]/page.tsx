831530679a058c974bf808b348276f056781c3cc
Michal Nowak <mich4ln@gmail.com>
2025-05-29 13:42:25 +0200

    ogloszenia fixes

--- app/galeria/[slug]/page.tsx
+++ app/galeria/[slug]/page.tsx
@@ -20,7 +20,6 @@ export async function generateStaticParams() {
 
 export default async function GalleryDetailPage({
   params,
-  searchParams,
 }: {
   params: { slug: string };
   searchParams?: { [key: string]: string | string[] | undefined };

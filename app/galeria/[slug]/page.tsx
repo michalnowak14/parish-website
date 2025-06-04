4498d00cef21db871af1515c82e434087b48e01f
Michal Nowak <mich4ln@gmail.com>
2025-06-04 11:07:20 +0200

    ogloszenia fixes

--- app/galeria/[slug]/page.tsx
+++ app/galeria/[slug]/page.tsx
@@ -16,7 +16,7 @@ export default async function Page({
 }: {
   params: Promise<{ slug: string }>;
 }) {
-  const { slug } = await params;
+  const slug = (await params).slug;
 
   const gallery: Gallery | null = await client.fetch(
     groq`*[_type == "gallery" && slug.current == $slug][0]{

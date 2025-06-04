8e30b450fb401aa764402e29167d6fabba4fa861
Michal Nowak <mich4ln@gmail.com>
2025-06-04 17:04:43 +0200

    added .vercelignore

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,5 +1,6 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
 
 export default defineConfig({
@@ -9,7 +10,7 @@ export default defineConfig({
   projectId: 'ps3zsjuw',
   dataset: 'production',
 
-  plugins: [structureTool()],
+  plugins: [structureTool(), visionTool()],
 
   schema: {
     types: schemaTypes,

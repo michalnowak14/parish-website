0586ef7a0b3082327babc2614d3d83e8215181e0
Michal <mich4ln@gmail.com>
2025-10-08 17:10:13 +0200

    back to old version

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -1,10 +1,8 @@
-// lib/sanity.client.ts
 import { createClient } from "next-sanity";
 
 export const client = createClient({
   projectId: "ps3zsjuw",
   dataset: "production",
-  apiVersion: "2025-01-01", // zawsze aktualna wersja API
-  useCdn: false, // wyłączamy cache, dane zawsze świeże
-  token: process.env.SANITY_API_READ_TOKEN, // jeśli dataset jest prywatny
+  apiVersion: "2024-01-01",
+  useCdn: false,
 });
